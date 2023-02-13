fda86527760ee0d7233f1d73fc66dc05f84a04be
Ido Preuss <ido.preuss@tipalti.com>
2023-02-13 16:04:21 +0200

    fix tests

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,7 +1,9 @@
-describe('Array', function() {
-    describe('#indexOf()', function() {
-      it('should return -1 when the value is not present', function() {
-        assert.equal([1, 2, 3].indexOf(4), -1);
-      });
+const assert = require('assert');
+describe('Simple Math Test', () => {
+ it('should return 2', () => {
+        assert.equal(1 + 1, 2);
     });
-  });
+ it('should return 9', () => {
+        assert.equal(3 * 3, 9);
+    });
+});
