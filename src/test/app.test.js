494f05dc43cfb07084534fed9414a3c160c304d2
Ido Preuss <ido.preuss@tipalti.com>
2023-02-13 18:47:34 +0200

    test api

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,9 +1,13 @@
 const assert = require('assert');
-describe('Simple Math Test', () => {
- it('should return 2', () => {
-        assert.equal(1 + 1, 2);
-    });
- it('should return 9', () => {
-        assert.equal(3 * 3, 9);
-    });
+const fetch = require('node-fetch');
+
+describe('External API Test', function() {
+  it('should return expected JSON from external API', async function() {
+    const expectedJson = { foo: 'bar', baz: 42 };
+
+    const response = await fetch('https://mockit.herokuapp.com/Cards');
+    const jsonResponse = await response.json();
+
+    assert.deepStrictEqual(jsonResponse, expectedJson);
+  });
 });
