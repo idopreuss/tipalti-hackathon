52195885dda25f7a66fef166536414cf37ac243a
Ido Preuss <ido.preuss@tipalti.com>
2023-02-13 17:54:54 +0200

    dynamic port

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
    res.json({key:"value"});
 });
 
-app.listen(8080, function() {
-  console.log('Web service listening on port 8080');
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
