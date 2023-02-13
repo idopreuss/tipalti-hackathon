4938ed509bb702b4daceeee990b01fb22405004f
Ido Preuss <ido.preuss@tipalti.com>
2023-02-13 17:47:29 +0200

    add endpoint

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ app.get('/cards', function(req, res) {
    res.json({key:"value"});
 });
 
+
+app.get('/', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+   res.json({key:"value"});
+});
+
 app.listen(8080, function() {
   console.log('Web service listening on port 8080');
 });
