d0eb94123fc5cb6830c3fb03a7fd363750e67556
Ido Preuss <ido.preuss@tipalti.com>
2023-02-13 20:59:34 +0200

    add timeout

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -6,7 +6,7 @@ describe('External API Test', function() {
   it('should return expected JSON from external API', async function() {
     const expectedJson = JSON.parse( JSON.stringify(card));
  
-    const response = await fetch('https://mockit.herokuapp.com/Cards');
+    const response = await fetch('https://mockit.herokuapp.com/Cards', { signal: AbortSignal.timeout(5000) });
     const jsonResponse = await response.json();
     
     console.log('expectedJson: ', expectedJson );
