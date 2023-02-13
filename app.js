ff15d6ab8c2b66fa9b0745f417b679f619ce55b4
Ido Preuss <ido.preuss@tipalti.com>
2023-02-13 15:40:38 +0200

    try1

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const app = express();
 
-const MongoClient = require("mongodb").MongoClient;
-
-// Connection URL
-const url = "mongodb+srv://idopreuss:P@rtner2809@meetingrooms.jgn0f7k.mongodb.net/?retryWrites=true&w=majority";
-
 app.get('/cards', function(req, res) {
   res.send('cards mock');
 });
